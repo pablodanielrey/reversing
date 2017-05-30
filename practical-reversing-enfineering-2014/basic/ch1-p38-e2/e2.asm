6b0265f7f6185c52e12a3f23d6605600cefd9797
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-05-29 23:47:39 -0300

    agregue otra forma de obtener el rip

--- practical-reversing-enfineering-2014/basic/ch1-p38-e2/e2.asm
+++ practical-reversing-enfineering-2014/basic/ch1-p38-e2/e2.asm
@@ -2,7 +2,16 @@
 section .text
 global _start
   _start:
+    jmp _f1
+
+  _f1:
     mov rax, _next
   _next:
+    TIMES 8 nop
+
+  _f2:
+    mov rax, [rip]
+    TIMES 8 nop
+
     mov rax, 0x3c
     syscall
